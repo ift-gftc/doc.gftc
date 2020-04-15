72346a856176abf4dc41e38f69a2d7efec010442
Karlos Santos <lgvichy@gmail.com>
2020-04-15 02:14:33 -0700

    Increase min-width, add word-wrap to code block

---
+++ src/gatsby-theme-apollo-docs/components/page-layout.js
@@ -0,0 +1,5 @@
+import PageLayout from "gatsby-theme-apollo-docs/src/components/page-layout"
+
+import "../code.less"
+export const NavItemsContext = PageLayout.NavItemsContext
+export default PageLayout
